8c21a3766b809e32e854702bed5203aa7e5dd822
yanhiz <yanis.dc@gmail.com>
2025-06-21 16:56:59 +0200

    New argument "delim on write_stimuli"

--- R/write_stimuli.R
+++ R/write_stimuli.R
@@ -5,6 +5,7 @@
 #'
 #' @param csvfile The path to the csv file containing your items.
 #' @param experiment_folder The path to the folder of your experiment.
+#' @param delim Delimitation symbol used in the .csv file. Default is ",".
 #' @examples
 #' write_stimuli('myexperiment/items.csv','/jatosfolder')
 
@@ -12,9 +13,9 @@
 #' @import tidyverse
 
 #' @export
-write_stimuli <- function(csv_file,experiment_folder){
+write_stimuli <- function(csv_file,experiment_folder,delim=','){
 
-  items <- read_csv(csv_file)
+  items <- read_delim(csv_file,delim=delim)
 
   stimuli <- paste('var stimuli =\n',jsonlite::toJSON(items,pretty = T))
 
