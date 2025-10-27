3795835755fb4b70ec5e7a6c8a530d956019986b
yanhiz <yanis.dc@gmail.com>
2025-10-27 10:11:01 +0100

    Writing stimuli from xls files is now possible

--- R/write_stimuli.R
+++ R/write_stimuli.R
@@ -1,31 +1,39 @@
 #' Write stimuli for Jatos
 #'
-#' This function converts a csv file containing experimental items into a stimuli.js file with JSON format.
+#' This function converts a xls/csv file containing experimental items into a stimuli.js file with JSON format.
 #' The stimuli.js file has to added in the Jatos folder of your experiment (in jatos/study_assets_root).
 #'
-#' @param csvfile The path to the csv file containing your items.
+#' @param stimuli_file The path to the xls/csv file containing your items.
 #' @param experiment_folder The path to the folder of your experiment.
+#' @param sheet Sheet selected from the xls file. Default is `1`.
 #' @param delim Delimitation symbol used in the .csv file. Default is ",".
 #' @param null_string Default `TRUE`. `TRUE` includes empty strings for unfilled columns of the stimuli csv file. `FALSE` removes unfilled columns.
 #'
 #' @examples
-#' write_stimuli('myexperiment/items.csv','/jatosfolder')
+#' write_stimuli('myexperiment/items.xls','/jatosfolder')
 #'
 #' @importFrom jsonlite toJSON
 #' @import tidyverse
 #'
 #' @export
-write_stimuli <- function(csv_file,experiment_folder,delim=',',null_string=TRUE){
+write_stimuli <- function(stimuli_file,experiment_folder,sheet=1,delim=',',null_string=TRUE){
   library(tidyverse)
+  library(readxl)
 
-  items <- read_delim(csv_file,delim=delim)
+  if (str_detect(stimuli_file,'\\.csv$')) {
+    items <- read_delim(stimuli_file,delim=delim)
+  } else {
+    items <- read_excel(stimuli_file,sheet=sheet)
+  }
 
-  if (null_string) {stimuli <- paste('var stimuli =\n',
+  if (null_string) {
+    stimuli <- paste('var stimuli =\n',
                                      jsonlite::toJSON(items,
                                                       na = "string",
                                                       pretty = T)) %>%
-                                      str_replace_all('"NA"','""')}
-  else {stimuli <- paste('var stimuli =\n',jsonlite::toJSON(items, pretty = T))}
+                                      str_replace_all('"NA"','""') } else {
+    stimuli <- paste('var stimuli =\n',jsonlite::toJSON(items, pretty = T))
+  }
 
   write_file(stimuli,file=paste0(experiment_folder,'/stimuli.js'))
 }
